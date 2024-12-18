99b921e4ecf2bafb463eaf7fddabf4c254222fe5
Anooj Basheer <anoojkm12@gmail.com>
2024-12-18 10:02:02 +0530

    created database and serializer class

---
+++ socialapp/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from socialapp.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    password1 = serializers.CharField(write_only = True)
+    
+    password2 = serializers.CharField(write_only = True)
+    
+    class Meta:
+        
+        model = User
+
+        fields = ["id","username","email","phone","password"]
+
+        read_only_fields=["id","password"]
+        
+    def create(self,validated_data):
+        
+        password1 = validated_data.pop("password1")
+        
+        password2 = validated_data.pop("password2")
+        
+        if password1!= password2:
+            
+            return serializers.ValidationError("password Missmatch")
+
+        return User.objects.create_user(**validated_data,password=password1)
+
+
+
--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "socialapp",
+    'restframework',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,23 @@ WSGI_APPLICATION = "socialmedia.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'socialappdb',
+		'USER': 'root',
+		'PASSWORD': '7515',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
